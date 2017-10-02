9a8abd9526b87b0f9fcc2453fb109bc108337423
Joe Vande Vusse <joevandevusse@gmail.com>
2017-10-02 00:28:40 -0500

    Not working? Added letters though

--- a_search.py
+++ a_search.py
@@ -12,8 +12,7 @@ def manhattan(a,b):
 	y2 = b.y
 	return abs(x1 - x2) + abs(y1 - y2)
 
-
-def a_search(graph, start, goal):
+def a_search(graph, start, goals):
 	my_heap = PriorityQueue()  #not really a heap just easier to call it that than queue
 	my_heap.push(start, 0)
 	pathway = []
@@ -21,41 +20,51 @@ def a_search(graph, start, goal):
 	came_from = {}
 	cost_so_far[start] = 0
 	came_from[start] = None
-	sum = 0
+	the_sum = 0
+	ordered_goals = []
 	while not my_heap.empty():
 		current = my_heap.pop()
 		#print("here")
-		if current in goal:
-			goal.remove(current)
-			for i in  graph:
+		if current in goals:
+			ordered_goals.append(current)
+			goals.remove(current)
+			for i in graph:
 				if i.parent:
-					sum += 1
+					the_sum += 1
 			while current.parent:
 				pathway.append(current)
 				x = current.parent
 				current.parent = None
 				current = x
-		if len(goal) == 0:
+		if len(goals) == 0:
 			break
 		for i in current.edges:
 			new_score = current.gscore + 1 #increment g(n)
 			if i not in cost_so_far or new_score < i.gscore:
 				cost_so_far[i] = new_score
 				i.gscore = new_score
-				i.hscore = findMST(i,goal)
+				i.hscore = findMST(i,goals)
 				#print(i.hscore, current)
 				i.fscore = i.gscore + i.hscore
 				my_heap.push(i, i.fscore)
 				if not i.parent:
 					i.parent = current
-
-	print("Nodes Expanded:", sum)
-	return pathway
+	return pathway, the_sum, ordered_goals
 
 
-graph, start, goal, print_out = makeMaze('mediumSearch.txt')
+graph, start, goals, print_out = makeMaze('tinySearch.txt')
+#graph, start, goals, print_out = makeMaze('smallSearch.txt')
+#graph, start, goals, print_out = makeMaze('mediumSearch.txt')
 meet_neighbors(graph)
-solution = a_search(graph, start, goal)
-print("Actual Solution:", len(solution))
-solution_maze = printSolution(solution, print_out)
+solution, nodes_expanded, goal_order = a_search(graph, start, goals)
+
+#solution_maze = printSolution(solution, print_out)
+solution_maze = printSolution(solution, print_out, goal_order)
 print(solution_maze)
+print("Nodes Expanded:", nodes_expanded)
+print("Solution Cost:", len(solution))
+
+# Bilal - for text file thing
+# When it's printing exactly when you want to the terminal
+# Simply do 'python3 a_search > filename.txt'
+# Just name the file differently for each maze/search
--- print_solution.py
+++ print_solution.py
@@ -1,11 +1,14 @@
-def printSolution(expanded_nodes, bare_maze):
+goal_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
+			 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def printSolution(expanded_nodes, bare_maze, goal_order=None):
 	#print(expanded_nodes)
 	#return
 	#unique_nodes = set(expanded_nodes)
 	solution_maze = []
 	line_len = 0
 	flag = 0
-
+	goal_count = 0
 	for i, char in enumerate(bare_maze):
 		if char == '\n' and flag == 0:
 			flag += 1
@@ -19,7 +22,11 @@ def printSolution(expanded_nodes, bare_maze):
 		if bare_maze[(line_len*node.y) + node.x] == 'P':
 			continue
 		elif bare_maze[(line_len*node.y) + node.x] == '.':
-			solution_maze[(line_len*node.y) + node.x] = '@'
+			if goal_order is not None:
+				solution_maze[(line_len*node.y) + node.x] = goal_list[goal_count]
+				goal_count += 1
+			else:
+				solution_maze[(line_len*node.y) + node.x] = '@'
 		else:
 			solution_maze[(line_len*node.y) + node.x] = '*'
 	solution_maze = "".join(solution_maze)
